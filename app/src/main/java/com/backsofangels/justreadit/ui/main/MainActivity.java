c9e0d873a8a7eedcc15ded2187044a17d39f5e29
backsofangels <backsofangels@gmail.com>
2019-05-08 23:48:47 +0200

    fix TabLayout rendering
    Fixed TabLayout rendering, polished the dependencies and renamed activity xml file.

---
+++ app/src/main/java/com/backsofangels/justreadit/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.backsofangels.justreadit.ui.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.backsofangels.justreadit.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_layout);
+    }
+}
