0cc16a3f30a12712118b220058638a0ac734ac0f
backsofangels <backsofangels@gmail.com>
2019-05-10 17:35:13 +0200

    create activity to display fragments
    Created the main activity XML layout with TabLayout and ViewPager.

--- app/src/main/java/com/backsofangels/justreadit/ui/main/MainActivity.java
+++ app/src/main/java/com/backsofangels/justreadit/ui/main/MainActivity.java
@@ -1,15 +1,21 @@
 package com.backsofangels.justreadit.ui.main;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.backsofangels.justreadit.R;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_layout);
+        tabLayout = (TabLayout) findViewById(R.id.main_tablayout);
+        viewPager = (ViewPager) findViewById(R.id.main_viewpager);
     }
 }
