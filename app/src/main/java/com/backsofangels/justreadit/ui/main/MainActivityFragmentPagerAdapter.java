e95f143ec339cd7c80e78aecf978ab686b9d519a
backsofangels <backsofangels@gmail.com>
2019-05-11 00:47:49 +0200

    Add FragmentPagerAdapter for MainActivity
    Added the FragmentPagerAdapter for MainActivity, without returns in the getItem section. The QRScanCodeFragment will just return an empty fragment, the ScanHistoryFragment will return the correct fragment instead. All the history functionality will be mocked up while working to it.

---
+++ app/src/main/java/com/backsofangels/justreadit/ui/main/MainActivityFragmentPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.backsofangels.justreadit.ui.main;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.widget.Toast;
+
+import com.backsofangels.justreadit.R;
+
+public class MainActivityFragmentPagerAdapter extends FragmentPagerAdapter {
+    private Context mContext;
+    private int tabsNumber = 4;
+
+    public MainActivityFragmentPagerAdapter(Context context, FragmentManager fmanager) {
+        super(fmanager);
+        this.mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                //return QRCodeScanFragment;
+            case 1:
+                //return ScanHistoryFragment;
+            default:
+                Toast.makeText(mContext, "Ops, something went wrong", Toast.LENGTH_LONG).show();
+        }
+
+        return new Fragment();
+    }
+
+    @Override
+    public int getCount() {
+        return tabsNumber;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position) {
+            case 0:
+                return mContext.getString(R.string.scancode_tab_title);
+            case 1:
+                return mContext.getString(R.string.scanhistory_tab_title);
+            default:
+                return "Default";
+        }
+    }
+}
